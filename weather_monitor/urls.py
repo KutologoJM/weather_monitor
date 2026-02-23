cff6eef4ea1dd681b8ab2f6cf994a51a532c5454
kutologojm <kutologomashego@gmail.com>
2026-02-23 21:56:22 +0800

    Updated django-health-check and added its dependencies

--- weather_monitor/urls.py
+++ weather_monitor/urls.py
@@ -14,45 +14,47 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from health_check.views import HealthCheckView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(f"health/{settings.HEALTH_CHECK_TOKEN}/",
-         HealthCheckView.as_view(
-        checks=[  # optional, default is all but 3rd party checks
-            "health_check.Cache",
-            "health_check.Database",
-            "health_check.Disk",
-            "health_check.Mail",
-            (
-                "health_check.Memory",
-                {  # tuple with options
-                    "min_gibibytes_available": 0.1,  # 0.1 GiB (~100 MiB)
-                    "max_memory_usage_percent": 80.0,
-                },
-            ),
-            "health_check.Storage",
-            # 3rd party checks
-            # "health_check.contrib.celery.Ping",
-            # "health_check.contrib.redis.Redis",
-        ],
-        use_threading=True,  # optional, default is True
-        warnings_as_errors=True,  # optional, default is True
-    ), name='health_check'),
+    path("admin/", admin.site.urls),
+    # Health Check docs https://codingjoe.dev/django-health-check/
+    path(
+        f"health/{settings.HEALTH_CHECK_TOKEN}/",
+        HealthCheckView.as_view(
+            checks=[
+                "health_check.DNS",
+                "health_check.Cache",
+                "health_check.Database",
+                "health_check.Mail",
+                "health_check.Storage",
+                # 3rd party checks
+                "health_check.contrib.psutil.Battery",
+                "health_check.contrib.psutil.CPU",
+                "health_check.contrib.psutil.Memory",
+                "health_check.contrib.psutil.Disk",
+                "health_check.contrib.psutil.Temperature",
+                # "health_check.contrib.celery.Ping",
+                # "health_check.contrib.redis.Redis",
+                # "health_check.contrib.atlassian.Cloudflare",
+                "health_check.contrib.atlassian.Render",
+            ]
+        ),
+    ),
     # path('accounts/', include('accounts.urls'))
-    path("weather/", include("weather.urls"))
+    path("weather/", include("weather.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-                      path(
-                          "__debug__/", include(debug_toolbar.urls)
-                      ),  # This registers the 'djdt' namespace
-                      path("__reload__/", include("django_browser_reload.urls")),
-                  ] + urlpatterns
+        path(
+            "__debug__/", include(debug_toolbar.urls)
+        ),  # This registers the 'djdt' namespace
+        path("__reload__/", include("django_browser_reload.urls")),
+    ] + urlpatterns
